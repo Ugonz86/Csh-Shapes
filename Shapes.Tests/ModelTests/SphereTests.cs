73942fa7eaad9a421d070cb37fb08b6724475276
Trevor Teigen and Uriel Gonzalez <trevor@>
2019-07-24 10:31:55 -0700

    add sphere testing

--- Shapes.Tests/ModelTests/SphereTests.cs
+++ Shapes.Tests/ModelTests/SphereTests.cs
@@ -1,35 +1,36 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Shapes.Models;
-//
-// namespace Shapes.Tests
-// {
-//   [TestClass]
-//   public class SphereTests
-//   {
-//
-//     [TestMethod]
-//     public void Sphere_GetVolume_VolumeOfSphere()
-//     {
-//       // Arrange
-//       Circle newCircle = new Circle(2);
-//       Sphere newSphere = new Sphere(newCircle);
-//       // Act
-//       int result = newSphere.GetVolume();
-//       // Assert
-//       Assert.AreEqual(result, 0);
-//     }
-//
-//     [TestMethod]
-//     public void Sphere_GetSurfaceArea_SurfaceAreaOfSphere()
-//     {
-//       // Arrange
-//       Circle newCircle = new Circle(2);
-//       Sphere newSphere = new Sphere(newCircle);
-//       // Act
-//       int result = newSphere.GetSurfaceArea();
-//       // Assert
-//       Assert.AreEqual(result, 96);
-//     }
-//
-//   }
-// }
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shapes.Models;
+using System;
+
+namespace Shapes.Tests
+{
+  [TestClass]
+  public class SphereTests
+  {
+
+    [TestMethod]
+    public void Sphere_GetVolume_VolumeOfSphere()
+    {
+      // Arrange
+      Circle newCircle = new Circle(2);
+      Sphere newSphere = new Sphere(newCircle);
+      // Act
+      double result = Math.Round(newSphere.GetVolume(),2);
+      // Assert
+      Assert.AreEqual(result, 25.13);
+    }
+
+    [TestMethod]
+    public void Sphere_GetSurfaceArea_SurfaceAreaOfSphere()
+    {
+      // Arrange
+      Circle newCircle = new Circle(2);
+      Sphere newSphere = new Sphere(newCircle);
+      // Act
+      double result = Math.Round(newSphere.GetSurfaceArea(),2);
+      // Assert
+      Assert.AreEqual(result, 50.27);
+    }
+
+  }
+}
